818c7a3f6c345440ae7d670839ff0aa3574e8b7b
Rafael <rafael.moreira.souza17@gmail.com>
2025-05-31 22:26:17 +0000

    xadrez novato

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,37 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    // Simulação do movimento da Torre (5 casas para a direita) usando for
+    printf("Movimento da Torre:\n");
+    int casas_torre = 5;
+    for (int i = 0; i < casas_torre; i++) 
+    {
+        printf("Direita\n");
+    }
+    printf("\n");
+
+    // Simulação do movimento do Bispo (5 casas na diagonal para cima e direita) usando while
+    printf("Movimento do Bispo:\n");
+    int casas_bispo = 5;
+    int contador_bispo = 0;
+    while (contador_bispo < casas_bispo) 
+    {
+        printf("Cima, Direita\n");
+        contador_bispo++;
+    }
+    printf("\n");
+
+    // Simulação do movimento da Rainha (8 casas para a esquerda) usando do-while
+    printf("Movimento da Rainha:\n");
+    int casas_rainha = 8;
+    int contador_rainha = 0;
+    do 
+    {
+        printf("Esquerda\n");
+        contador_rainha++;
+    } while (contador_rainha < casas_rainha);
+
+    
+
     return 0;
 }
